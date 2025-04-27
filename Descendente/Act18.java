a3b9e85e499b5c60cf32ead13b79c58d13a89a57
Federico <poupeau04@gmail.com>
2025-04-27 15:45:52 -0300

    act18

--- Descendente/Act18.java
+++ Descendente/Act18.java
@@ -1,3 +1,20 @@
 public class Act18 {
-    
+    public static void main(String[] args) {
+        funcion();
+    }
+    public static void funcion() {
+        for (int numero = 2; numero <= 10000; numero++) {
+            int sumaDivisores = 0;
+            
+            for (int i = 1; i < numero; i++) {
+                if (numero % i == 0) {
+                    sumaDivisores += i;
+                }
+            }
+            
+            if (sumaDivisores == numero) {
+                System.out.println(numero + " es un número perfecto.");
+            }
+        }
+    }
 }

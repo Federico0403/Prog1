711b5506a7b0f03e14986804e25c5f053390ae1a
Federico <poupeau04@gmail.com>
2025-05-07 12:21:02 -0300

    Act31

---
+++ Arreglos/Act31.java
@@ -0,0 +1,75 @@
+package Arreglos;
+
+public class Act31 {
+    final static int MAX = 10;
+    final static int SEPARADOR = 0;
+    public static void main(String[] args) {
+        int[]arr = {0,2,0,4,3,1,0,2,4,0};
+        
+        int inicio = 0; // para que entre una vez al while 
+        int fin = -1;//tengo que poner -1 ya que en buscar inicio busca desde fin+1
+
+        int anteultimaSec = contadorSecuencias(arr)-1;
+        int contadorSec = 0;
+
+        while (inicio < MAX) {
+            inicio = buscarInicio(arr, fin+1);
+            if (inicio < MAX) {
+                fin = buscarFin(arr, inicio);
+                contadorSec++;
+                if (anteultimaSec == contadorSec) {
+                    invertirSec(arr, inicio, fin);
+                }
+            }
+        }
+        mostrarArreglo(arr);
+    }
+
+    public static void invertirSec(int[] arr, int inicio, int fin) {
+        while (inicio < fin) {
+            int temp = arr[inicio];
+            arr[inicio] = arr[fin];
+            arr[fin] = temp;
+            inicio++;
+            fin--;
+        }
+    }
+    
+
+    public static int contadorSecuencias(int[]arr){
+        int contador = 0;
+
+        int inicio = 0; // para que entre una vez al while 
+        int fin = -1;//tengo que poner -1 ya que en buscar inicio busca desde fin+1
+
+        while (inicio < MAX) {
+            inicio = buscarInicio(arr, fin+1);
+            if (inicio < MAX) {
+                fin = buscarFin(arr, inicio);
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+
+    public static int buscarInicio(int[]arr, int pos){
+        while (pos < MAX && arr[pos] == SEPARADOR) {
+            pos++;
+        }
+        return pos;
+    }
+    public static int buscarFin(int[]arr, int pos){
+        while (pos < MAX && arr[pos] != SEPARADOR) {
+            pos++;
+        }
+        return pos-1;
+    }
+    public static void mostrarArreglo(int arr[]){
+        for (int pos = 0; pos < MAX; pos++) {
+            System.out.print("|"+arr[pos]+"|");
+        }
+    }
+}
+
+

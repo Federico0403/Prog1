9a2e9b8b5241529666a1ae4d55613d1b154653d1
Federico <poupeau04@gmail.com>
2025-05-20 15:38:58 -0300

    Act5

--- Arreglos/EjTipoParcial5.java
+++ Arreglos/EjTipoParcial5.java
@@ -2,45 +2,45 @@ package Arreglos;
 
 public class EjTipoParcial5 {
 
-    // MAXE pedidos de empanadas
     final static int MAXE = 16;
-    final static int SEPARADOR = 0;// (NUMEROS negativos)
+    final static int SEPARADOR = 0;
 
     public static void main(String[] args) {
 
-        // arreglo de empanadas
         int[] arrE = { -1, 5, 5, 7, 7, -6, 4, 9, 10, -4, 1, 1, 2, 7, -9, -8 };
 
-        // si el cliente ingresa un 7 para en el pedido 2
-        // asi deberia quedar el arr E {-1 5 5 7 7 -6 4 7 9 10 -4 1 1 2 7 -9}
-
-        int empanada = 7;
-        int pedido = 2;
+        int empanada = 10;
+        int pedido = 3; // Cambiá entre 1, 2, 3, etc.
 
         int inicio = 0;
         int fin = -1;
 
         int contarSecuencia = 0;
+        int posicionAcolocarEmpanada;
+        boolean pedidoEncontrado = false;
 
-        int posicionAcolocarEmpanada = 0;
-
-        while (inicio < MAXE) {
-            inicio = buscarFin(arrE, fin + 1);
-            if (inicio < MAXE) {
+        while (inicio < MAXE && !pedidoEncontrado) {
+            inicio = buscarInicio(arrE, fin + 1);
+            if (inicio < MAXE && !pedidoEncontrado) {
                 fin = buscarFin(arrE, inicio);
                 contarSecuencia++;
+                System.out.println("s" + contarSecuencia);
+
                 if (pedido == contarSecuencia) {
                     posicionAcolocarEmpanada = encontrarPosicion(arrE, inicio, fin, empanada);
                     corrimientoDerecha(arrE, posicionAcolocarEmpanada);
                     arrE[posicionAcolocarEmpanada] = empanada;
-                } else {
-                    System.out.println("no se encontro ese pedido");
+                    pedidoEncontrado = true;
                 }
             }
             inicio = fin + 1;
         }
-        mostrarArreglo(arrE);
 
+        if (!pedidoEncontrado) {
+            System.out.println("No se encontró ese pedido.");
+        }
+
+        mostrarArreglo(arrE);
     }
 
     public static void corrimientoDerecha(int[] arr, int desde) {
@@ -52,7 +52,7 @@ public class EjTipoParcial5 {
     public static int encontrarPosicion(int[] arrE, int inicio, int fin, int empanada) {
         int i = inicio;
         boolean posicionEncontrada = false;
-        int posicion = 0;
+        int posicion = fin + 1; // por defecto al final porsi empanada es un numero grande y ninguno lo supera
 
         while (i <= fin && !posicionEncontrada) {
             if (empanada < arrE[i]) {
@@ -73,7 +73,7 @@ public class EjTipoParcial5 {
 
     public static int buscarFin(int[] arr, int fin) {
         if (fin < 0)
-            fin = 0; // corregimos valores negativos
+            fin = 0;
         while (fin < MAXE && arr[fin] >= SEPARADOR) {
             fin++;
         }
